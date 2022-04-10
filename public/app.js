01cdd39296971002f4b202f0dab62dfbb1eb32fb
Alissa <alissamak@outlook.com>
2022-04-10 16:37:35 -0400

    completed assignment reqs

--- public/app.js
+++ public/app.js
@@ -1,151 +1,100 @@
-// foursquare key: fsq3883VuFRctHJpVP1ZKXU5feHeGKjhWQtREQmPUxlWm8o=
-//get user location
+// set empty global variables latitude and longitute
+let lat;
+let lng;
+
+//set map
 const myMap = L.map('map');
-async function userCoord(lat, lng){
+
+//async function to get user location, businesses, and business type in one map
+async function userCoord(){
     const location = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
     })
+    
+    //consoling the user location details and coordinates
     console.log(location);
     console.log([location.coords.latitude, location.coords.longitude]);
-    //get lat and lng coords
+    
+    //get user lat and lng coords
     lat = location.coords.latitude
     lng = location.coords.longitude
-    // create map based on coords
-    myMap.setView([lat, lng],15);
-    // myMap.locate({setView: true, maxZoom: 16})
     
-    //create coords marker
+    //creates map based on user coords
+    myMap.setView([lat, lng],15);
+    
+    //creates user coords marker
     const marker = L.marker([lat, lng])
     marker.addTo(myMap).bindPopup('<p1><b>You are Here!</b></p1>').openPopup()
     
-    //returns person coords to the map
-    return myMap;
-};
-//call coords
-userCoord();
-
-// add openstreetmap tiles to map
-const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //addes map layer to show user coords
+    const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: '5',
 }).addTo(myMap);
 
-//add eventListener for businesses
-const coffee = document.querySelector('#coffee');
-const restaurant = document.querySelector('#restaurant');
-const hotel = document.querySelector('#hotel');
-const market = document.querySelector('#market');
-const selections = document.querySelectorAll('.business')
-// console.log(selections)
-
-let goBtn = document.querySelector('.go-btn');
-// goBtn.forEach(business => {
-//     business.addEventListener('click', () => {
-//         getBusiness(business.value)
+    //function for fetching foursquare apis for each business type
+    async function getBusiness(business, lat, lng){
+        lat = location.coords.latitude
+        lng = location.coords.longitude
         
-//     })
-//     console.log(business.value)
-//     console.log(business)
-// })
-// let coffeeLayer = coffee.baseLayer
-// let restaurantLayer = restaurant.baseLayer
-// let hotelLayer = hotel.baseLayer
-// let marketLayer = market.baseLayer
+        const options = {
+            method: 'GET',
+            headers: {
+            Accept: 'application/json',
+            Authorization: 'fsq3883VuFRctHJpVP1ZKXU5feHeGKjhWQtREQmPUxlWm8o='
+            }
+        };
 
-
-// goBtn.addEventListener('click', () => {
-//     let coffeeLayer = coffee.addTo(myMap)
-//     getBusiness(coffeeLayer)
-// })
-
-
-//fetching foursquare apis
-async function getBusiness(business, lat, lon, limit){
-    // businessArray = ["coffee", "restaurant", "hotel", "market"]
-    // businessArray.forEach(element => {
-    //     console.log(element)
-    // })
-   
-    const options = {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          Authorization: 'fsq3883VuFRctHJpVP1ZKXU5feHeGKjhWQtREQmPUxlWm8o='
-        }
-      };
-      
-    // let response = await fetch(`https://api.foursquare.com/v3/places/search?query=${business}&ll=${lat}%2C${lon}&limit=${limit}`, options)     
-    let response = await fetch(`https://api.foursquare.com/v3/places/search?query=coffee&ll=35.83%2C-79.02&limit=10`, options) 
-    let data = await response.text();
-    let parsedData = JSON.parse(data);
-    business = parsedData.results;
-    console.log(business);
-    //adding markers for each business
-    business.forEach(element => {
-        let businessLat = element.geocodes.main.latitude;
-        let businessLng = element.geocodes.main.longitude;
-        let businessName = element.name;
-        // console.log(element.geocodes.main)
-        let businessCoords = [businessLat, businessLng]
+        //fetching data with dyanmic link based on business, lat, and lng
+        let response = await fetch(`https://api.foursquare.com/v3/places/search?query=${business}&ll=${lat}%2C${lng}&limit=10`, options)     
+        let data = await response.text();
+        let parsedData = JSON.parse(data);
+        let businesses = parsedData.results;
         
-        const marker = L.marker(businessCoords)
-        marker.addTo(myMap).bindPopup(businessName).openPopup()
-    });
-    
-    // coffee.addEventListener('click', () => {
-    //     this.getcoffee()
-    // }); 
-    
-}
-// getBusiness()
-
-
-// async function getcoffee(){
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//           Accept: 'application/json',
-//           Authorization: 'fsq3883VuFRctHJpVP1ZKXU5feHeGKjhWQtREQmPUxlWm8o='
-//         }
-//       };
-      
-//     // let response = fetch(`https://api.foursquare.com/v3/places/search?query=${business}&ll=${lat}%2C${lon}&radius=10000&limit=${limit}`, options)     
-//     let response = await fetch('https://api.foursquare.com/v3/places/search?query=coffee&ll=35.83%2C-79.02&limit=10', options) 
-//     let data = await response.text();
-//     let parsedData = JSON.parse(data);
-//     let coffee = parsedData.results;
-//     console.log(coffee);
-//     coffee.forEach(element => {
-//         let coffeeLat = element.geocodes.main.latitude;
-//         let coffeeLng = element.geocodes.main.longitude;
-//         let coffeeName = element.name;
-//         // console.log(element.geocodes.main)
-//         let coffeeCoords = [coffeeLat, coffeeLng]
+        //logs businesses in the area
+        console.log(businesses);
         
-//         const marker = L.marker(coffeeCoords)
-//         marker.addTo(myMap).bindPopup(coffeeName).openPopup()
-//     });
-//     // return coffee;
-
-// }
-// getcoffee();
-// // fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`)
-
-// //adding layer controls
-// const coffees = L.layerGroup([]).addTo(myMap); 
-// const restaurants = L.layerGroup([]).addTo(myMap);
-// const hotels = L.layerGroup([]).addTo(myMap);
-// const markets = L.layerGroup([]).addTo(myMap);
-
-// var baseMaps = {
-//     "main": baseLayer
-// };
+        //adding markers for each business
+        businesses.forEach(business => {
+            let businessLat = business.geocodes.main.latitude;
+            let businessLng = business.geocodes.main.longitude;
+            let businessName = business.name;
+            // console.log(element.geocodes.main)
+            let businessCoords = [businessLat, businessLng]
+            
+            const marker = L.marker(businessCoords)
+            marker.addTo(myMap).bindPopup(businessName).openPopup()
+        });
+    }
     
-// var overlayMaps = {
-//     "coffee": coffees,
-//     "restaurant": restaurants,
-//     "hotel": hotels,
-//     "market": markets,
-// };
-
-// L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+    //function to get business type when selected in dropdown menu and to call getBusiness function
+    function getSelection(){
+        let selection = document.querySelectorAll('#business-selections')
+        //cycles through each value in selection options
+        selection.forEach(element => {
+            let businessArray = ["coffee", "restaurant", "hotel", "market"]
+            //if coffee is selected in dropdown, calls getBusiness() for coffee
+            if(element.value === businessArray[0]){
+                getBusiness(businessArray[0])
+            }
+            //if restaurant is selected in dropdown, calls getBusiness() for coffee
+            if(element.value === businessArray[1]){
+                getBusiness(businessArray[1])
+            }
+            //if hotel is selected in dropdown, calls getBusiness() for coffee
+            if(element.value === businessArray[2]){
+                getBusiness(businessArray[2])
+            }
+            //if market is selected in dropdown, calls getBusiness() for coffee
+            if(element.value === businessArray[3]){
+                getBusiness(businessArray[3])
+            }
+            //logs which element is showing when selected in dropdown
+            console.log(element.value)
+        })
+    }
+    //calling selections for dropdown business selection
+    getSelection()
+};
+//calling all the functions and coords in userCoord function
+userCoord();
